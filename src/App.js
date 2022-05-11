ad18341b9e2758c45c8a2db2cfd3fa8d0e27ee68
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-12 00:35:40 +0700

    basic of routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import React from 'react';
+
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './index.css'
 import App from './App';
 import { createRoot } from 'react-dom/client';
 // import App from './final/App';
