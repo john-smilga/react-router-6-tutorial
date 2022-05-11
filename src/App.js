b3f9eb5bbf36910f88bac8da961569f67de14f44
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-11 11:06:17 +0700

    set up with react 18

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return <h2>React Router 6 Tutorial</h2>;
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 // import App from './final/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <App />
+)
