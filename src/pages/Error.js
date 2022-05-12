26964f85237ebd7e771b65d64df1cfba2b12ef7b
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-12 22:30:46 +0700

    adjust error page to display 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to='/' className="btn">Back home</Link>
     </section>
   );
 };
