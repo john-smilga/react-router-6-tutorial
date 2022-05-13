0b56ce12fbac49a7d892f8439075cc6fd787f3d7
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-13 21:50:15 +0700

    render single product based on product id

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,13 +1,21 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   // use useParams() in order to get access to the params in the url, which in this case is the product id
   // The object key "productId" need to match with the :productId which we set in the Route path
   const { productId } = useParams();
+
+  //Base on the productId we got from the url, use that id to find the related product in data
+  const product = products.find(product => productId === product.id);
+  //extract the image and name property of the product object
+  const { name, image } = product;
+
   return (
     <section className='section product'>
-      <h2>{productId}</h2>
-      <Link to='/products' className="btn"> Back to Products Page</Link>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to='/products' className="btn"> Back to Products</Link>
     </section>
   );
 };
