21abd500bf97f10f3f59942e63c520da317dc25f
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-13 21:41:02 +0700

    display all products to products page from data

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+import products from '../data';
+
 const Products = () => {
   return (
     <>
       <section className='section'>
         <h2>products</h2>
+        <div className='products'>
+          {products.map(product => {
+            const { id, name } = product;
+            return (
+              <article key={id}>
+                <h5 >{name}</h5>
+                <Link to={`/products/${id}`}>More info</Link>
+              </article>
+            )
+          })}
+        </div>
       </section>
     </>
   );
